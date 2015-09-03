e01a01b961f82a8f7f0d76477753a05727a60ddf
markusos <markus.ostberg@gmail.com>
2015-09-02 23:11:56 -0400

    Updated example code

--- README.md
+++ README.md
@@ -2,6 +2,36 @@
 
 A minimal web scraper using Guzzle and PHP's DOM parser.
 
+###Example
+
+```
+<?php
+
+$scraper = new Scrape('http://markusos.github.io/');
+$scraper->load('/');
+
+$posts = $scraper->getNodes('//ul[@class="post-list"]/li');
+
+$data = [];
+
+foreach($posts as $post) {
+	$postLinkElement = $scraper->getNode('./h2/a[@class="post-link"]', $post);
+	$dateElement = $scraper->getNode('./span[@class="post-meta"]', $post);
+	$excerptElement = $scraper->getNode('./p', $post);
+
+	$data[] = [
+		'link' => $postLinkElement->getAttribute('href');,
+		'title' => $postLinkElement->nodeValue,
+		'date' => $dateElement->nodeValue,
+		'excerpt' => $excerptElement->nodeValue
+	];
+}
+
+var_dump($data);
+
+
+```
+
 ###License
 
 The MIT License (MIT)
--- test/ScrapeTest.php
+++ test/ScrapeTest.php
@@ -8,26 +8,28 @@ class ScrapeTest extends \PHPUnit_Framework_TestCase
     }
 
     public function testBasicScrape() {
+
         $scraper = new Scrape('http://markusos.github.io/');
         $scraper->load('/');
 
-        $siteTitle = $scraper->getNode('//a[@class="site-title"]');
-        echo $siteTitle->nodeValue . PHP_EOL;
-
         $posts = $scraper->getNodes('//ul[@class="post-list"]/li');
-        echo "----------------" . PHP_EOL;
+
+        $data = [];
 
         foreach($posts as $post) {
             $postLinkElement = $scraper->getNode('./h2/a[@class="post-link"]', $post);
             $dateElement = $scraper->getNode('./span[@class="post-meta"]', $post);
             $excerptElement = $scraper->getNode('./p', $post);
 
-            echo $postLinkElement->nodeValue . PHP_EOL;
-            echo $postLinkElement->getAttribute('href') . PHP_EOL;
-            echo $dateElement->nodeValue . PHP_EOL;
-            echo $excerptElement->nodeValue . PHP_EOL;
-            echo "----------------" . PHP_EOL;
+            $data[] = [
+                'link' => $postLinkElement->getAttribute('href'),
+                'title' => $postLinkElement->nodeValue,
+                'date' => $dateElement->nodeValue,
+                'excerpt' => $excerptElement->nodeValue
+            ];
         }
+
+        var_dump($data);
     }
 
     /**
